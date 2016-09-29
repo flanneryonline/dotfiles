8ded2c2b2b7ed7101f5784a558a76631dbb635fd
Pat Flannery <pat@flanneryonline.com>
2016-09-29 14:52:49 -0500

    removing link from old repo

--- dot.sh
+++ dot.sh
@@ -2,17 +2,17 @@
 
 #vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/server/dotfiles/dot.vimrc ~/.vimrc
+ln -s ~/dotfiles/dot.vimrc ~/.vimrc
 
 #zsh
-ln -s ~/server/dotfiles/dot.zshrc ~/.zshrc
+ln -s ~/dotfiles/dot.zshrc ~/.zshrc
 
 #tmux
-ln -s ~/server/dotfiles/dot.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/dot.tmux.conf ~/.tmux.conf
 
 #freebsd base stuff
-ln -s ~/server/dotfiles/dot.shrc ~/.shrc
-ln -s ~/server/dotfiles/dot.login.conf ~/.login.conf
-ln -s ~/server/dotfiles/dot.login.conf.db ~/.login.conf.db
-ln -s ~/server/dotfiles/dot.profile ~/.profile
+ln -s ~/dotfiles/dot.shrc ~/.shrc
+ln -s ~/dotfiles/dot.login.conf ~/.login.conf
+ln -s ~/dotfiles/dot.login.conf.db ~/.login.conf.db
+ln -s ~/dotfiles/dot.profile ~/.profile
 
