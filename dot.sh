dd94a584e01d45a2fc6dd8e09f7c3bbde59cbe71
Patrick Flannery <Patrick Flannery>
2017-01-08 12:11:19 -0600

    fix

--- dot.sh
+++ dot.sh
@@ -4,22 +4,23 @@
 get_local="`dirname "$0"`"
 cd "`cd -P "$get_local" && pwd `"
 
-#vim
-curl -fLo ../.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s dot.vimrc ../.vimrc
-
-#zsh
-ln -s dot.zshrc ../.zshrc
-
-#tmux
-ln -s dot.tmux.conf ../.tmux.conf
-
-#freebsd stuff
 for file in ../.cshrc ../.login_conf ../.login_conf.db ../.profile ../.shrc
 do
     [ -f $file ] && rm $file
     [ -L $file ] && rm $file
 done
+
+#vim
+curl -fLo ../.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s dotfiles/dot.vimrc ../.vimrc
+
+#zsh
+ln -s dotfiles/dot.zshrc ../.zshrc
+
+#tmux
+ln -s dotfiles/dot.tmux.conf ../.tmux.conf
+
+#freebsd stuff
 ln -s dotfiles/dot.cshrc ../.cshrc
 ln -s dotfiles/dot.login_conf ../.login_conf
 ln -s dotfiles/dot.login_conf.db ../.login_conf.db
