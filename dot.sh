d3e48024ff071290846e41962c0f5cbc5a723e32
Patrick Flannery <Patrick Flannery>
2017-01-08 14:02:42 -0600

    silence output

--- dot.sh
+++ dot.sh
@@ -10,7 +10,7 @@ do
 done
 
 #vim
-curl -fLo ../.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -sfLo ../.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s dotfiles/dot.vimrc ../.vimrc
 
 #zsh
